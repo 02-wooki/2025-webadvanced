6583ee85ec6f995796cf6aa1a53a0f61ae19e096
wooki <hyunook02@naver.com>
2025-04-08 09:24:22 +0900

    4주차 과제

--- week4/src/App.js
+++ week4/src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import NotificationList from "./NotificationList"; // ✅ NotificationList 추가
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>알림 리스트</h1>
+                <NotificationList /> {/* ✅ 알림 리스트 렌더링 */}
+            </header>
+        </div>
+    );
 }
 
 export default App;
