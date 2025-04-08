b82354f9bbb41480adae66772e0145dff18fd32f
wooki <hyunook02@naver.com>
2025-04-08 09:45:17 +0900

    useState 실습

--- week6/src/App.js
+++ week6/src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import UseStateEx from "./Components/useStateComponents/useStateTest";
+import NonUseStateTest from './Components/useStateComponents/nonUseStateTest';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <UseStateEx></UseStateEx>
+      <NonUseStateTest></NonUseStateTest>
     </div>
   );
 }
