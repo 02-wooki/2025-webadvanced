4fed18f3b76a5e98214b554c9282efdcbe4d1930
wooki <hyunook02@naver.com>
2025-04-10 21:29:58 +0900

    6주차 수업 실습

--- week6/src/App.js
+++ week6/src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
-import UseStateEx from "./Components/useStateComponents/useStateTest";
-import NonUseStateTest from './Components/useStateComponents/nonUseStateTest';
+import UseStateEx from "./Components/useState/useStateTest";
+import NonUseStateTest from './Components/useState/nonUseStateTest';
+import Area from "./Components/area";
 
 function App() {
   return (
     <div>
       <UseStateEx></UseStateEx>
       <NonUseStateTest></NonUseStateTest>
+      <Area />
     </div>
   );
 }
