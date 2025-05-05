a91e27df5d82e55cecd9d0e1e606700d8127b9f3
wooki <hyunook02@naver.com>
2025-05-05 12:51:28 +0900

    9주차

---
+++ 0429/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import ButtonComponent from './components/ButtonComponent';
+import InputComponent from './components/InputComponent';
+import Parent from './components/Parent'
+import MyButton from './components/MyButton'
+
+function App() {
+  return (
+    <>
+      <ButtonComponent></ButtonComponent>
+      <InputComponent></InputComponent>
+      <Parent></Parent>
+      <MyButton></MyButton>
+    </>
+  );
+}
+
+export default App;
